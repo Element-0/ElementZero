0bd4b6e2d41608da1a501b9beec1e034ffb6e2d4
CodeHz <CodeHz@outlook.com>
2020-02-17 23:40:52 +0800

    fix multi-hook

--- ModLoader/hook_impl.cpp
+++ ModLoader/hook_impl.cpp
@@ -1,17 +1,31 @@
 #include "pch.h"
 #include <hook.h>
 #include "hook_init.h"
+#include <map>
 
 funchook *funchook_instance = funchook_create();
+
+static std::map<void *, void **> hookmap;
+
 int HookFunction(void *oldfunc, void **poutold, void *newfunc) {
+  auto source = oldfunc;
+  auto it = hookmap.find(oldfunc);
+  if (it != hookmap.end()) {
+    *poutold = *it->second;
+    *it->second = newfunc;
+    return 0;
+  }
   auto code = funchook_prepare(funchook_instance, &oldfunc, newfunc);
-  if (poutold) *poutold = oldfunc;
+  if (code == 0) {
+    *poutold = oldfunc;
+    hookmap.emplace(source, poutold);
+  }
   return code;
 }
 
 void *GetServerSymbol(char const *name) {
   static auto handle = GetModuleHandle(nullptr);
-  return (void*)GetProcAddress(handle, name);
+  return (void *) GetProcAddress(handle, name);
 }
 
 FailedToHook::FailedToHook(int code) : exception(funchook_error_message(funchook_instance)) {}
