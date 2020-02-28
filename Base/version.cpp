9464f47ac36ef85cf09f4b76e2ef2208a784994d
CodeHz <CodeHz@outlook.com>
2020-02-28 09:58:07 +0800

    add version details

--- Base/version.cpp
+++ Base/version.cpp
@@ -1,8 +1,12 @@
 #include <hook.h>
 #include <string>
 
+#ifndef EZVERSION
+#  define EZVERSION ""
+#endif
+
 THook(
     std::string,
     "?getServerVersionString@Common@@YA?AV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@XZ") {
-  return original() + " with element-0";
+  return original() + (" with element-0 (" EZVERSION ")");
 }
---
+++ cmake/Utility.cmake
@@ -0,0 +1,21 @@
+macro (generate_git_version _out_var)
+    find_package (Git QUIET)
+    if (GIT_FOUND)
+        execute_process (
+            COMMAND ${GIT_EXECUTABLE} describe --abbrev=6 --dirty --always --tags
+            OUTPUT_VARIABLE ${_out_var}
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_QUIET
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        )
+        if (${_out_var} STREQUAL "")
+            set (${_out_var} "unknown")
+        endif ()
+    endif ()
+endmacro ()
+
+function (find_and_install_package name)
+    find_package (${name} CONFIG REQUIRED)
+    get_target_property (dll_files ${name} IMPORTED_LOCATION_RELEASE)
+    install (FILES ${dll_files} DESTINATION .)
+endfunction ()
