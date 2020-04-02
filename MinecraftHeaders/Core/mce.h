8e0a748755ee24e96568fc538e6c6205aca8c9da
codehz <codehz@outlook.com>
2020-04-03 00:06:19 +0800

    improve mce::UUID

--- MinecraftHeaders/Core/mce.h
+++ MinecraftHeaders/Core/mce.h
@@ -9,7 +9,17 @@ class UUID {
 public:
   std::uint64_t a, b;
 
+  UUID() {}
+  UUID(std::uint64_t a, std::uint64_t b) : a(a), b(b) {}
+  UUID(UUID const &rhs) : a(rhs.a), b(rhs.b) {}
+
   constexpr inline bool operator==(UUID const &rhs) const noexcept { return a == rhs.a && b == rhs.b; }
+
+  UUID &operator=(UUID const &rhs) {
+    a = rhs.a;
+    b = rhs.b;
+    return *this;
+  }
 
   inline operator char const *() const { return (char const *) this; }
 
