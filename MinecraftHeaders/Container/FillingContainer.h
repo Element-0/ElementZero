bc6b82a589a15d479977fe370cfc9062b11e9a5d
codehz <codehz@outlook.com>
2020-04-11 19:51:11 +0800

    [Header] add FillingContainer

---
+++ MinecraftHeaders/Container/FillingContainer.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Container.h"
+#include "ContainerType.h"
+#include "../Core/SemVersion.h"
+#include "../Level/Level.h"
+
+#include "../dll.h"
+
+class FillingContainer : public Container {
+public:
+  std::vector<ItemStack> data;
+  Player *player;
+
+  MCAPI FillingContainer(Player *, int, ContainerType);
+  MCAPI ~FillingContainer() override;
+  MCAPI virtual ItemStack const &getItem(int) const override;
+  MCAPI virtual void setItem(int, ItemStack const &) override;
+  MCAPI virtual int getContainerSize() const override;
+  MCAPI virtual int getMaxStackSize() const override;
+  MCAPI virtual void startOpen(Player &) override;
+  MCAPI virtual void stopOpen(Player &) override;
+
+  MCAPI virtual bool add(ItemStack &);
+  MCAPI virtual bool canAdd(ItemStack const &);
+  MCAPI virtual void dropAll(bool);
+  MCAPI virtual void clearSlot(int);
+  MCAPI virtual void clearInvertory(int);
+  MCAPI virtual void load(ListTag const &, SemVersion const &, Level &);
+  MCAPI virtual void doDrop(ItemStack &, bool);
+  MCAPI virtual int getEmptySlotsCount() const;
+  MCAPI virtual void dropAll(int, int, bool);
+};
+
+static_assert(offsetof(FillingContainer, data) == 224);
+static_assert(offsetof(FillingContainer, player) == 248);
