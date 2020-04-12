f3a9ad1d1e2b29c50ff730b9a6dda5286f9d200a
codehz <codehz@outlook.com>
2020-04-12 19:53:59 +0800

    [Header] format

--- MinecraftHeaders/Block/BlockSource.h
+++ MinecraftHeaders/Block/BlockSource.h
@@ -4,13 +4,14 @@
 
 class BlockSource {
 public:
-	inline BlockSource(const BlockSource&);
-	inline BlockSource& operator=(const BlockSource&);
-	inline BlockSource();
-inline bool isConsideredSolidBlock(class BlockPos const& a0) {
-return CallServerClassMethod<bool>("?isConsideredSolidBlock@BlockSource@@QEAA_NAEBVBlockPos@@@Z", this, &a0);
-	}
-inline class Block const& getBlock(class BlockPos const& a0) const {
-	return CallServerClassMethod<class Block const&>("?getBlock@BlockSource@@QEBAAEBVBlock@@AEBVBlockPos@@@Z", this, &a0);
-}
+  inline BlockSource(const BlockSource &);
+  inline BlockSource &operator=(const BlockSource &);
+  inline BlockSource();
+  inline bool isConsideredSolidBlock(class BlockPos const &a0) {
+    return CallServerClassMethod<bool>("?isConsideredSolidBlock@BlockSource@@QEAA_NAEBVBlockPos@@@Z", this, &a0);
+  }
+  inline class Block const &getBlock(class BlockPos const &a0) const {
+    return CallServerClassMethod<class Block const &>(
+        "?getBlock@BlockSource@@QEBAAEBVBlock@@AEBVBlockPos@@@Z", this, &a0);
+  }
 };
