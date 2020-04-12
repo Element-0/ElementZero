2ee30afcdb1fec0b3fe2342c7c27163de035f9b9
codehz <codehz@outlook.com>
2020-04-12 12:28:30 +0800

    [Scripting] add setTimeout and setInterval

---
+++ BuiltinMods/ScriptingSupport/apiset/timer.cpp
@@ -0,0 +1,46 @@
+#include <sstream>
+
+#include "ChakraCommon.h"
+#include "chakra_helper.h"
+#include "log.h"
+#include "scheduler.h"
+
+#include <scriptapi.h>
+
+using namespace Mod::Scripting;
+
+static RegisterQueue queue("timer", [](JsObjectWarpper global) {
+  global["setTimeout"] = [](JsValueRef callee, Arguments args) -> JsValueRef {
+    if (args.size() != 1 && args.size() != 2) throw std::runtime_error{"Require 1 or 2 arguments"};
+    auto fn = args[0];
+    if (GetJsType(fn) != JsFunction) throw std::runtime_error{"The first argument require to be function"};
+    auto delay = args.size() == 2 ? FromJs<int>(args[1]) : 0;
+    auto token =
+        Mod::Scheduler::SetTimeOut(Mod::Scheduler::GameTick(delay), [holder{ValueHolder{fn}}](unsigned long long) {
+          JsValueRef ref{GetUndefined()};
+          JsCallFunction(*holder, &ref, 1, nullptr);
+        });
+    return ToJs((double) token);
+  };
+  global["clearTimeout"] = +[](double tok) {
+    Mod::Scheduler::Token token = (size_t) tok;
+    Mod::Scheduler::ClearTimeOut(token);
+  };
+
+  global["setInterval"] = [](JsValueRef callee, Arguments args) -> JsValueRef {
+    if (args.size() != 1 && args.size() != 2) throw std::runtime_error{"Require 1 or 2 arguments"};
+    auto fn = args[0];
+    if (GetJsType(fn) != JsFunction) throw std::runtime_error{"The first argument require to be function"};
+    auto delay = args.size() == 2 ? FromJs<int>(args[1]) : 0;
+    auto token =
+        Mod::Scheduler::SetInterval(Mod::Scheduler::GameTick(delay), [holder{ValueHolder{fn}}](unsigned long long) {
+          JsValueRef ref{GetUndefined()};
+          JsCallFunction(*holder, &ref, 1, nullptr);
+        });
+    return ToJs((double) token);
+  };
+  global["clearInterval"] = +[](double tok) {
+    Mod::Scheduler::Token token = (size_t) tok;
+    Mod::Scheduler::ClearInterval(token);
+  };
+});
