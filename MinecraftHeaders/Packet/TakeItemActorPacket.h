e8b04524078bf6d9b624f951cc24e08ef2d2f113
codehz <codehz@outlook.com>
2020-04-17 18:26:55 +0800

    [Header] Add TakeItemActorPacket

---
+++ MinecraftHeaders/Packet/TakeItemActorPacket.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../Core/Packet.h"
+#include "../Actor/ActorRuntimeID.h"
+#include "../dll.h"
+
+class TakeItemActorPacket : public Packet {
+public:
+  ActorRuntimeID entity, target;
+
+  inline ~TakeItemActorPacket() {}
+  MCAPI virtual MinecraftPacketIds getId() const;
+  MCAPI virtual std::string getName() const;
+  MCAPI virtual void write(BinaryStream &) const;
+  MCAPI virtual PacketReadResult read(ReadOnlyBinaryStream &);
+};
