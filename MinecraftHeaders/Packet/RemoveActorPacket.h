a84b8a04f51c1068efad291d9108e16322196ad3
codehz <codehz@outlook.com>
2020-04-17 18:14:40 +0800

    [Header] Add RemoveActorPacket

---
+++ MinecraftHeaders/Packet/RemoveActorPacket.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../Core/Packet.h"
+#include "../Actor/ActorUniqueID.h"
+#include "../dll.h"
+
+class RemoveActorPacket : public Packet {
+public:
+  ActorUniqueID uid;
+
+  inline ~RemoveActorPacket() {}
+  MCAPI virtual MinecraftPacketIds getId() const;
+  MCAPI virtual std::string getName() const;
+  MCAPI virtual void write(BinaryStream &) const;
+  MCAPI virtual PacketReadResult read(ReadOnlyBinaryStream &);
+};
