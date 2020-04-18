ad6919cb16e36bdda5fa4dada7cf8911226bec2b
codehz <codehz@outlook.com>
2020-04-18 15:07:36 +0800

    [Header] Add ItemFrameDropItemPacket

---
+++ MinecraftHeaders/Packet/ItemFrameDropItemPacket.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../Core/Packet.h"
+#include "../Math/BlockPos.h"
+#include "../dll.h"
+
+class ItemFrameDropItemPacket : public Packet {
+public:
+  BlockPos pos;
+
+  inline ~ItemFrameDropItemPacket() {}
+  MCAPI virtual MinecraftPacketIds getId() const;
+  MCAPI virtual std::string getName() const;
+  MCAPI virtual void write(BinaryStream &) const;
+  MCAPI virtual PacketReadResult read(ReadOnlyBinaryStream &);
+};
