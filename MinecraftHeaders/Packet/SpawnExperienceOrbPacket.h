cbf4c3852cd8cb4fbba21f7711d0bac9248e7337
codehz <codehz@outlook.com>
2020-04-18 14:18:13 +0800

    [Header] Add SpawnExperienceOrbPacket

---
+++ MinecraftHeaders/Packet/SpawnExperienceOrbPacket.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../Core/Packet.h"
+#include "../Math/Vec3.h"
+#include "../dll.h"
+
+class SpawnExperienceOrbPacket : public Packet {
+public:
+  alignas(8) Vec3 pos;
+  int amount;
+
+  inline ~SpawnExperienceOrbPacket() {}
+  MCAPI virtual MinecraftPacketIds getId() const;
+  MCAPI virtual std::string getName() const;
+  MCAPI virtual void write(BinaryStream &) const;
+  MCAPI virtual PacketReadResult read(ReadOnlyBinaryStream &);
+};
+
+static_assert(offsetof(SpawnExperienceOrbPacket, pos) == 40);
+static_assert(offsetof(SpawnExperienceOrbPacket, amount) == 52);
