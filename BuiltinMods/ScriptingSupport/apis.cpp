c150e19b1eb391e282be56d97fe276279212d49a
codehz <codehz@outlook.com>
2020-04-19 16:57:06 +0800

    [ScriptSupport] Fix promise

--- BuiltinMods/ScriptingSupport/apis.cpp
+++ BuiltinMods/ScriptingSupport/apis.cpp
@@ -2,6 +2,7 @@
 
 #include <log.h>
 #include <scriptapi.h>
+#include <scheduler.h>
 
 #include "global.h"
 
@@ -9,8 +10,17 @@ using namespace Mod::Scripting;
 
 void installConsoleAPI(JsObjectWarpper &global);
 
+void PromiseContinuationCallback(JsValueRef task, void *callbackState) {
+  using namespace Mod::Scheduler;
+  Mod::Scheduler::SetTimeOut(0_tick, [holder{ValueHolder{task}}](auto) {
+    JsValueRef ref{GetUndefined()};
+    JsCallFunction(*holder, &ref, 1, nullptr);
+  });
+}
+
 void initBasicAPI() {
   DEF_LOGGER("ScriptAPI");
+  JsSetPromiseContinuationCallback(PromiseContinuationCallback, nullptr);
   auto global             = JsObjectWarpper::FromGlobal();
   global["isElementZero"] = true;
   for (auto [name, fn] : RegisterQueue::GetList()) {
