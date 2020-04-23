248d050b8e74f6fc2bc3ff565fb32006a7f8a13a
codehz <codehz@outlook.com>
2020-04-23 17:32:05 +0800

    [Hook] find symbols from addition symbols

--- ModLoader/hook_impl.cpp
+++ ModLoader/hook_impl.cpp
@@ -1,6 +1,11 @@
+#include "SQLiteCpp/Database.h"
+#include "SQLiteCpp/Statement.h"
+#include "boost/scope_exit.hpp"
+#include "modutils.h"
 #include "pch.h"
 #include <hook.h>
 #include "hook_init.h"
+#include <SQLiteCpp/SQLiteCpp.h>
 #include <map>
 
 funchook *funchook_instance = funchook_create();
@@ -9,9 +14,9 @@ static std::map<void *, void **> hookmap;
 
 int HookFunction(void *oldfunc, void **poutold, void *newfunc) {
   auto source = oldfunc;
-  auto it = hookmap.find(oldfunc);
+  auto it     = hookmap.find(oldfunc);
   if (it != hookmap.end()) {
-    *poutold = *it->second;
+    *poutold    = *it->second;
     *it->second = newfunc;
     return 0;
   }
@@ -25,7 +30,21 @@ int HookFunction(void *oldfunc, void **poutold, void *newfunc) {
 
 void *GetServerSymbol(char const *name) {
   static auto handle = GetModuleHandle(nullptr);
-  return (void *) GetProcAddress(handle, name);
+  static SQLite::Database db{"addition_symbols.db"};
+  static SQLite::Statement stmt{db, "SELECT rva FROM symbols WHERE symbol=?"};
+  if (auto ret = (void *) GetProcAddress(handle, name); ret) return ret;
+  BOOST_SCOPE_EXIT_ALL() {
+    stmt.tryReset();
+    stmt.clearBindings();
+  };
+  try {
+    stmt.bindNoCopy(1, name);
+    if (stmt.executeStep()) {
+      printf("@@@@ %lld\n", stmt.getColumn(0).getInt64());
+      return (void *) ((uint64_t) stmt.getColumn(0).getInt64() + (uint64_t) handle);
+    }
+    return 0;
+  } catch (...) { return 0; }
 }
 
 FailedToHook::FailedToHook(int code) : exception(funchook_error_message(funchook_instance)) {}
