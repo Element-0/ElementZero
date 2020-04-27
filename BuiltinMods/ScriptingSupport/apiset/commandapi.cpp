d3d391002deda6ae43721f47836aecc795abb7b2
codehz <codehz@outlook.com>
2020-04-27 18:14:23 +0800

    [Scripting] Remove comments

--- BuiltinMods/ScriptingSupport/apiset/commandapi.cpp
+++ BuiltinMods/ScriptingSupport/apiset/commandapi.cpp
@@ -13,17 +13,6 @@ static ModuleRegister reg("ez:command", [](JsObjectWrapper native) -> std::strin
   native["executeCommand"] = +[](std::string const &command) {
     return ToJs(
         Mod::CommandSupport::GetInstance().ExecuteCommand(std::make_unique<Mod::CustomCommandOrigin>(), command));
-    // auto origin      = std::make_unique<Mod::CustomCommandOrigin>();
-    // origin->callback = [holder = ValueHolder{cb}](Json::Value &&value) mutable {
-    //   try {
-    //     auto json         = ToJs(value);
-    //     JsValueRef args[] = {GetUndefined(), json};
-    //     JsCallFunction(*holder, args, 2, nullptr);
-    //   } catch (...) {}
-    // };
-    // auto ctx    = CommandContext::create(data, std::move(origin));
-    // auto result = LocateService<MinecraftCommands>()->executeCommand(std::move(ctx), false);
-    // return result == MCRESULT_Success;
   };
   return R"js(
     export const executeCommand = import.meta.native.executeCommand;
