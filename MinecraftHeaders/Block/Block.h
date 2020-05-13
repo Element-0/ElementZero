da06cbb6d4455cb924f09177c0184ce13290a97d
codehz <codehz@outlook.com>
2020-05-13 18:21:29 +0800

    [Header] fix Block

--- MinecraftHeaders/Block/Block.h
+++ MinecraftHeaders/Block/Block.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#include "../Core/WeakPtr.h"
+#include "../Core/NBT.h"
+#include "BlockLegacy.h"
 #include "../dll.h"
 
 class Block {
 public:
+  unsigned short aux;
+  WeakPtr<BlockLegacy> legacy;
+  CompoundTag definition;
+  int runtime_id;
+  bool has_runtime_id;
+
   MCAPI virtual ~Block();
   MCAPI virtual enum BlockRenderLayer getRenderLayer() const;
 };
