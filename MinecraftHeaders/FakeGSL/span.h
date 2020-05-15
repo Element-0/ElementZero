ec7555729873ac21717e7cb361d2d5ef3c37984f
codehz <codehz@outlook.com>
2020-05-15 22:10:40 +0800

    [Header] Add FakeGSL

---
+++ MinecraftHeaders/FakeGSL/span.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+
+namespace gsl {
+
+template <typename T, size_t ext = (size_t) -1> class span;
+
+template <typename T> class span<T, (size_t) -1> {
+public:
+  T *a, *b;
+
+  T *begin() const { return a; }
+  T *end() const { return b; }
+  bool empty() const { return a == b; }
+  size_t size() const { return b - a; }
+};
+
+} // namespace gsl
