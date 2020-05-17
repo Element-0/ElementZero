25ed08b9fb6315f9f8622035eaf8362696e31e77
codehz <codehz@outlook.com>
2020-05-17 15:11:13 +0800

    [NetworkStatus] Initial commit

---
+++ BuiltinMods/NetworkStatus/dllmain.cpp
@@ -0,0 +1,49 @@
+#include "playerdb.h"
+#include <Net/NetworkHandler.h>
+#include <Net/NetworkIdentifier.h>
+#include <Actor/Player.h>
+
+#include <dllentry.h>
+#include <command.h>
+#include <base.h>
+#include <scriptapi.h>
+#include <log.h>
+
+DEF_LOGGER("NetworkStatus");
+
+struct NetworkStatus : Command {
+  void execute(const CommandOrigin &orig, CommandOutput &outp) override {
+    if (orig.getOriginType() != CommandOriginType::Player) {
+      outp.error("commands.generic.error.invalidPlayer", {"/balance"});
+      return;
+    }
+    auto ent    = (Player *) orig.getEntity();
+    auto &netid = ent->getNetworkIdentifier();
+    auto peer   = LocateService<NetworkHandler>()->getPeerForUser(netid);
+    auto status = peer->getNetworkStatus();
+    outp.success("commands.net-stat.result", {status.ping, status.avgping, status.packetloss, status.avgpacketloss});
+  }
+
+  static void setup(CommandRegistry *reg) {
+    reg->registerCommand(
+        "net-stat", "commands.net-stat.description", CommandPermissionLevel::Any, CommandFlagCheat, CommandFlagNone);
+    reg->registerOverload<NetworkStatus>("net-stat");
+  }
+};
+
+void PreInit() { Mod::CommandSupport::GetInstance().AddListener(SIG("loaded"), &NetworkStatus::setup); }
+
+using namespace Mod::Scripting;
+static ModuleRegister reg("ez:network-stats", [](JsObjectWrapper native) -> std::string {
+  native["getNetworkStats"] = +[](Mod::PlayerEntry const &player) {
+    auto peer   = LocateService<NetworkHandler>()->getPeerForUser(player.netid);
+    auto status = peer->getNetworkStatus();
+    JsObjectWrapper ret;
+    ret["ping"]          = status.ping;
+    ret["avgping"]       = status.avgping;
+    ret["packetloss"]    = status.packetloss;
+    ret["avgpacketloss"] = status.avgpacketloss;
+    return *ret;
+  };
+  return "export const getNetworkStats = import.meta.native.getNetworkStats;";
+});
