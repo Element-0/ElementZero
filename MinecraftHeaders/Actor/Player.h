b7eb808b79190c52eab60703555dd4454ef842d1
CodeHz <codehz@outlook.com>
2020-06-24 13:36:00 +0800

    [MinecraftHeader] Fix Player

--- MinecraftHeaders/Actor/Player.h
+++ MinecraftHeaders/Actor/Player.h
@@ -12,12 +12,14 @@
 #include "../Math/Vec3.h"
 #include "../Math/BlockPos.h"
 #include "../Core/AutomaticID.h"
+#include "../Core/VanillaDimensions.h"
 #include "../Command/CommandPermissionLevel.h"
 #include "../dll.h"
 
 class Packet;
 class ServerPlayer;
 class NetworkIdentifier;
+class Dimension;
 enum class StructureFeatureType;
 enum class AbilitiesIndex;
 enum class BedSleepingResult;
@@ -33,6 +35,27 @@ enum class PlayerUISlot;
 
 class Player : public Mob {
 public:
+  class PlayerSpawnPoint {
+  public:
+    inline static BlockPos InvalidSpawnPos = BlockPos::MIN;
+    BlockPos spawn_block = InvalidSpawnPos, spawn_pos = InvalidSpawnPos;
+    AutomaticID<class Dimension, int> dim = VanillaDimensions::Undefined;
+
+    inline PlayerSpawnPoint()        = default;
+    inline PlayerSpawnPoint &operator=(PlayerSpawnPoint &&) = default;
+    inline bool hasSpawnPoint() const { return spawn_block != BlockPos::MIN && dim != VanillaDimensions::Undefined; }
+    inline void invalidate() { *this = {}; }
+    inline bool isValid() const { return dim != VanillaDimensions::Undefined; }
+    inline void setSpawnPoint(BlockPos spawn_block, AutomaticID<class Dimension, int> dim, BlockPos spawn_pos) {
+      this->spawn_block = spawn_block;
+      this->spawn_pos   = spawn_pos;
+      this->dim         = dim;
+    }
+    inline void setSpawnPoint(BlockPos spawn_block, AutomaticID<class Dimension, int> dim) {
+      setSpawnPoint(spawn_block, dim, spawn_block);
+    }
+  };
+
   inline ServerPlayer *asServerPlayer() const noexcept {
     return const_cast<ServerPlayer *>(reinterpret_cast<ServerPlayer const *>(this));
   }
@@ -47,7 +70,6 @@ public:
   MCAPI virtual void frameUpdate(class FrameUpdateContextBase &) = 0;
   MCAPI virtual std::vector<class ChunkPos> const &getTickingOffsets(void) const;
   MCAPI virtual void moveView(void);
-  MCAPI virtual void moveSpawnView(class Vec3 const &);
   MCAPI virtual void setName(std::string const &);
   MCAPI virtual void checkMovementStats(class Vec3 const &);
   MCAPI virtual enum StructureFeatureType getCurrentStructureFeature(void) const;
@@ -59,41 +81,15 @@ public:
   MCAPI virtual bool hasResource(int);
   MCAPI virtual void completeUsingItem(void);
   MCAPI virtual void setPermissions(enum CommandPermissionLevel);
-  MCAPI virtual void startCrafting(class BlockPos const &, bool);
-  MCAPI virtual void startStonecutting(class BlockPos const &);
   MCAPI virtual void startDestroying(void);
   MCAPI virtual void stopDestroying(void);
-  MCAPI virtual void openContainer(class BlockPos const &);
-  MCAPI virtual void openContainer(struct ActorUniqueID const &);
-  MCAPI virtual void openFurnace(class BlockPos const &);
-  MCAPI virtual void openBlastFurnace(class BlockPos const &);
-  MCAPI virtual void openSmoker(class BlockPos const &);
-  MCAPI virtual void openEnchanter(class BlockPos const &);
-  MCAPI virtual void openAnvil(class BlockPos const &);
-  MCAPI virtual void openGrindstone(class BlockPos const &);
-  MCAPI virtual void openBrewingStand(class BlockPos const &);
-  MCAPI virtual void openHopper(class BlockPos const &);
-  MCAPI virtual void openHopper(struct ActorUniqueID const &);
-  MCAPI virtual void openDispenser(class BlockPos const &, bool);
-  MCAPI virtual void openBeacon(class BlockPos const &);
   MCAPI virtual void openPortfolio(void);
   MCAPI virtual void openBook(int, bool, int, class BlockActor *);
-  MCAPI virtual void openCommandBlock(class BlockPos const &);
-  MCAPI virtual void openCommandBlockMinecart(struct ActorUniqueID const &);
-  MCAPI virtual void openHorseInventory(struct ActorUniqueID const &);
   MCAPI virtual void openTrading(struct ActorUniqueID const &, bool);
   MCAPI virtual bool canOpenContainerScreen(void);
   MCAPI virtual void openChalkboard(class ChalkboardBlockActor &, bool);
   MCAPI virtual void openNpcInteractScreen(class Actor &);
   MCAPI virtual void openInventory(void);
-  MCAPI virtual void openStructureEditor(class BlockPos const &);
-  MCAPI virtual void openLabTable(class BlockPos const &);
-  MCAPI virtual void openElementConstructor(class BlockPos const &);
-  MCAPI virtual void openCompoundCreator(class BlockPos const &);
-  MCAPI virtual void openMaterialReducer(class BlockPos const &);
-  MCAPI virtual void openLoom(class BlockPos const &);
-  MCAPI virtual void openStonecutter(class BlockPos const &);
-  MCAPI virtual void openCartographyTable(class BlockPos const &);
   MCAPI virtual void displayChatMessage(std::string const &, std::string const &);
   MCAPI virtual void displayClientMessage(std::string const &);
   MCAPI virtual void displayLocalizableMessage(std::string const &, class std::vector<std::string> const &);
@@ -146,80 +142,6 @@ public:
   MCAPI virtual enum ClientPlayMode getPlayMode(void) const       = 0;
   MCAPI virtual void reportMovementTelemetry(enum MovementEventType);
   MCAPI virtual void onMovePlayerPacketNormal(class Vec3 const &, class Vec2 const &, float);
-
-  // SHARED
-  MCAPI virtual void feed(int);
-  MCAPI virtual void aiStep(void);
-  MCAPI virtual void remove(void);
-  MCAPI virtual float getSpeed(void) const;
-  MCAPI virtual void rideTick(void);
-  MCAPI virtual void setSpeed(float);
-  MCAPI virtual void travel(float, float, float);
-  MCAPI virtual bool useNewAi(void) const;
-  MCAPI virtual bool isJumping(void) const;
-  MCAPI virtual void normalTick(void);
-  MCAPI virtual bool isBlocking(void) const;
-  MCAPI virtual bool isCreative(void) const;
-  MCAPI virtual bool isImmobile(void) const;
-  MCAPI virtual bool isSleeping(void) const;
-  MCAPI virtual bool isAdventure(void) const;
-  MCAPI virtual bool isShootable(void);
-  MCAPI virtual void stopSwimming(void);
-  MCAPI virtual bool consumeTotem(void);
-  MCAPI virtual void dropEquipment(void);
-  MCAPI virtual void setSleeping(bool);
-  MCAPI virtual void startSwimming(void);
-  MCAPI virtual void updateGliding(void);
-  MCAPI virtual void jumpFromGround(void);
-  MCAPI virtual void move(class Vec3 const &);
-  MCAPI virtual void stopSpinAttack(void);
-  MCAPI virtual float getCameraOffset(void) const;
-  MCAPI virtual float getRidingHeight(void);
-  MCAPI virtual bool isWorldBuilder(void);
-  MCAPI virtual void sendInventory(bool);
-  MCAPI virtual void startSpinAttack(void);
-  MCAPI virtual int getPortalCooldown(void) const;
-  MCAPI virtual int getPortalWaitTime(void) const;
-  MCAPI virtual bool attack(class Actor &);
-  MCAPI virtual int getItemUseDuration(void);
-  MCAPI virtual bool add(class ItemStack &);
-  MCAPI virtual void onSynchedDataUpdate(int);
-  MCAPI virtual bool getAlwaysShowNameTag(void) const;
-  MCAPI virtual void adjustDamageAmount(int &) const;
-  MCAPI virtual bool drop(class ItemStack const &, bool);
-  MCAPI virtual bool isCreativeModeAllowed(void);
-  MCAPI virtual bool canAddRider(class Actor &) const;
-  MCAPI virtual bool canBePulledIntoVehicle(void) const;
-  MCAPI virtual bool canExistWhenDisallowMob(void) const;
-  MCAPI virtual float getMapDecorationRotation(void) const;
-  MCAPI virtual void sendMotionPacketIfNeeded(void);
-  MCAPI virtual void teleportTo(class Vec3 const &, bool, int, int);
-  MCAPI virtual float getItemUseStartupProgress(void);
-  MCAPI virtual void awardKillScore(class Actor &, int);
-  MCAPI virtual void die(class ActorDamageSource const &);
-  MCAPI virtual float getItemUseIntervalProgress(void);
-  MCAPI virtual class ItemStack const &getCarriedItem(void) const;
-  MCAPI virtual enum ActorType getEntityTypeId(void) const;
-  MCAPI virtual void setCarriedItem(class ItemStack const &);
-  MCAPI virtual void setOffhandSlot(class ItemStack const &);
-  MCAPI virtual class ItemStack const &getEquippedTotem(void) const;
-  MCAPI virtual void handleEntityEvent(enum ActorEvent, int);
-  MCAPI virtual class HashedString const &getActorRendererId(void) const;
-  MCAPI virtual void setArmor(enum ArmorSlot, class ItemStack const &);
-  MCAPI virtual void dropEquipment(class ActorDamageSource const &, int);
-  MCAPI virtual void actuallyHurt(int, class ActorDamageSource const *, bool);
-  MCAPI virtual class Vec3 getAttachPos(enum ActorLocation, float) const;
-  MCAPI virtual bool isDamageBlocked(class ActorDamageSource const &) const;
-  MCAPI virtual bool isInvulnerableTo(class ActorDamageSource const &) const;
-  MCAPI virtual void useItem(class ItemStack &, enum ItemUseMethod, bool);
-  MCAPI virtual class AnimationComponent &getAnimationComponent(void);
-  MCAPI virtual enum CommandPermissionLevel getCommandPermissionLevel(void) const;
-  MCAPI virtual void reloadHardcoded(enum Actor::InitializationMethod, class VariantParameterList const &);
-  MCAPI virtual class std::unique_ptr<class Packet> getAddPacket(void);
-  MCAPI virtual class std::vector<class ItemStack const *> getAllHand(void) const;
-  MCAPI virtual void initializeComponents(enum Actor::InitializationMethod, class VariantParameterList const &);
-  MCAPI virtual class std::vector<class ItemStack const *> getAllEquipment(void) const;
-  MCAPI virtual std::string getFormattedNameTag(void) const;
 
   bool isInRaid(void) const;
   bool isSurvival(void) const;
--- MinecraftHeaders/Math/BlockPos.h
+++ MinecraftHeaders/Math/BlockPos.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Vec3.h"
+#include <limits>
 
 class BlockPos {
 public:
@@ -8,8 +9,16 @@ public:
   // For ABI
   inline ~BlockPos() {}
 
+  static BlockPos MIN;
+  static BlockPos MAX;
+
   BlockPos operator+(BlockPos const &rhs) const noexcept { return {x + rhs.x, y + rhs.y, z + rhs.z}; }
   constexpr bool operator==(BlockPos const &rhs) const noexcept { return x == rhs.x && y == rhs.y && z == rhs.z; }
   constexpr bool operator!=(BlockPos const &rhs) const noexcept { return !(*this == rhs); }
   operator Vec3() { return {(float) x, (float) y, (float) z}; }
 };
+
+inline BlockPos BlockPos::MIN = {
+    std::numeric_limits<int>::min(), std::numeric_limits<int>::min(), std::numeric_limits<int>::min()};
+inline BlockPos BlockPos::MAX = {
+    std::numeric_limits<int>::max(), std::numeric_limits<int>::max(), std::numeric_limits<int>::max()};
