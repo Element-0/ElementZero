4caee6802df10a778899d840563fe20a4a7feb8b
CodeHz <codehz@outlook.com>
2020-06-24 23:38:40 +0800

    [ScriptSupport] Fix patch

--- BuiltinMods/ScriptingSupport/dllmain.cpp
+++ BuiltinMods/ScriptingSupport/dllmain.cpp
@@ -3,20 +3,12 @@
 
 #include "global.h"
 
-void dllenter() {}
-void dllexit() {}
-
 void PreInit() {
   mod_enabled = true;
   GetServerSymbolWithOffset<PatchSpan<6>>(
-      "?onServerThreadStarted@MinecraftServerScriptEngine@@UEAA?AW4EventResult@@AEAVServerInstance@@@Z", 0x39)
-      ->VerifyPatchFunction({0x0F, 0x84, 0x59, 0x01, 0x00, 0x00}, NopFilled{});
-
+      "?onServerThreadStarted@MinecraftServerScriptEngine@@UEAA?AW4EventResult@@AEAVServerInstance@@@Z", 0x49)
+      ->VerifyPatchFunction({0x0F, 0x84, 0x85, 0x01, 0x00, 0x00}, NopFilled{});
   GetServerSymbolWithOffset<PatchSpan<6>>(
-      "?onServerThreadStarted@MinecraftServerScriptEngine@@UEAA?AW4EventResult@@AEAVServerInstance@@@Z", 0x46)
-      ->VerifyPatchFunction({0x0F, 0x84, 0x4C, 0x01, 0x00, 0x00}, NopFilled{});
-
-  GetServerSymbolWithOffset<PatchSpan<6>>(
-      "?onServerThreadStarted@MinecraftServerScriptEngine@@UEAA?AW4EventResult@@AEAVServerInstance@@@Z", 0x56)
-      ->VerifyPatchFunction({0x0F, 0x84, 0x3C, 0x01, 0x00, 0x00}, NopFilled{});
+      "?onServerThreadStarted@MinecraftServerScriptEngine@@UEAA?AW4EventResult@@AEAVServerInstance@@@Z", 0x59)
+      ->VerifyPatchFunction({0x0F, 0x84, 0x75, 0x01, 0x00, 0x00}, NopFilled{});
 }
