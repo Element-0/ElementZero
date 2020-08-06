983a6f635b887622bb970ece1ed3f3ee0d3755c7
CodeHz <codehz@outlook.com>
2020-08-06 09:14:17 +0800

    [Header] fix ActorBlock

--- MinecraftHeaders/Block/ActorBlock.h
+++ MinecraftHeaders/Block/ActorBlock.h
@@ -4,15 +4,5 @@
 
 class ActorBlock : public BlockLegacy {
 public:
-  MCAPI virtual ~ActorBlock() override;
-  MCAPI virtual bool playerWillDestroy(class Player &, class BlockPos const &, class Block const &) const override;
-  MCAPI virtual void
-  neighborChanged(class BlockSource &, class BlockPos const &, class BlockPos const &) const override;
-  MCAPI virtual void
-  spawnResources(class BlockSource &, class BlockPos const &, class Block const &, float, int) const override;
-  MCAPI virtual void triggerEvent(class BlockSource &, class BlockPos const &, int, int) const override;
   MCAPI virtual class ItemInstance getEntityResourceItem(class Random &, class BlockActor const &, int) const;
-
-protected:
-  MCAPI virtual bool canBeSilkTouched(void) const override;
 };
