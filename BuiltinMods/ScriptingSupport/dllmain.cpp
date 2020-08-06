f49a4effa32e6f9888476b63dac1a4fe58c4b544
CodeHz <codehz@outlook.com>
2020-08-06 11:57:20 +0800

    [ScriptSupport] Fix hard-coded patch

--- BuiltinMods/ScriptingSupport/dllmain.cpp
+++ BuiltinMods/ScriptingSupport/dllmain.cpp
@@ -6,9 +6,12 @@
 void PreInit() {
   mod_enabled = true;
   GetServerSymbolWithOffset<PatchSpan<6>>(
+      "?onServerThreadStarted@MinecraftServerScriptEngine@@UEAA?AW4EventResult@@AEAVServerInstance@@@Z", 0x3C)
+      ->VerifyPatchFunction({0x0F, 0x84, 0x5F, 0x01, 0x00, 0x00}, NopFilled{});
+  GetServerSymbolWithOffset<PatchSpan<6>>(
       "?onServerThreadStarted@MinecraftServerScriptEngine@@UEAA?AW4EventResult@@AEAVServerInstance@@@Z", 0x49)
-      ->VerifyPatchFunction({0x0F, 0x84, 0x85, 0x01, 0x00, 0x00}, NopFilled{});
+      ->VerifyPatchFunction({0x0F, 0x84, 0x52, 0x01, 0x00, 0x00}, NopFilled{});
   GetServerSymbolWithOffset<PatchSpan<6>>(
       "?onServerThreadStarted@MinecraftServerScriptEngine@@UEAA?AW4EventResult@@AEAVServerInstance@@@Z", 0x59)
-      ->VerifyPatchFunction({0x0F, 0x84, 0x75, 0x01, 0x00, 0x00}, NopFilled{});
+      ->VerifyPatchFunction({0x0F, 0x84, 0x42, 0x01, 0x00, 0x00}, NopFilled{});
 }
