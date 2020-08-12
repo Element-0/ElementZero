d4577c285ea4bd9681c8836620dcb76fff08c279
CodeHz <codehz@outlook.com>
2020-08-13 03:10:27 +0800

    [deps] try fix sqlite3

--- cmake/Deps.cmake
+++ cmake/Deps.cmake
@@ -1,5 +1,8 @@
 find_package (SQLiteCpp CONFIG REQUIRED)
-find_and_install_package (sqlite3 sqlite3)
+find_file (SQLITE3DLL
+    NAMES sqlite3.dll
+    HINTS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/bin")
+install (FILES SQLITE3DLL DESTINATION .)
 find_and_install_package (yaml-cpp yaml-cpp)
 find_package (msgpack CONFIG REQUIRED)
 find_package (Flatbuffers CONFIG REQUIRED)
